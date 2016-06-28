012e5a5329fb4b247992ff9ba75c4c5903f39804
Tim Baker <bakert@us.ibm.com>
2016-06-28 14:55:22 -0500

    script to recreate the bluemix data tables

--- env-setup/bluemix-scripts/reset-cloudant-tables.sh
+++ env-setup/bluemix-scripts/reset-cloudant-tables.sh
@@ -9,6 +9,6 @@ curl -X PUT https://820923e0-be08-46f5-a34a-003f91f00f5c-bluemix:10d585c237c8d7b
 curl -X PUT https://820923e0-be08-46f5-a34a-003f91f00f5c-bluemix:10d585c237c8d7b599b79cfcca39cb63356f2cea7d79abf27f284801b3c149d9@820923e0-be08-46f5-a34a-003f91f00f5c-bluemix.cloudant.com/review
 
 # Populate item Database
-for filename in ./*.json; do
+for filename in ./data/*.json; do
   curl -H "Content-Type: application/json" -d @"$filename" https://820923e0-be08-46f5-a34a-003f91f00f5c-bluemix:10d585c237c8d7b599b79cfcca39cb63356f2cea7d79abf27f284801b3c149d9@820923e0-be08-46f5-a34a-003f91f00f5c-bluemix.cloudant.com/item
 done
