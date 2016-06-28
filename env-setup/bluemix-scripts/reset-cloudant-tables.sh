85726e84890726213c1af06337d26dfa25f9dff9
Tim Baker <bakert@us.ibm.com>
2016-06-28 14:50:51 -0500

    script to recreate the bluemix data tables

---
+++ env-setup/bluemix-scripts/reset-cloudant-tables.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Delete Databases
+curl -X DELETE https://820923e0-be08-46f5-a34a-003f91f00f5c-bluemix:10d585c237c8d7b599b79cfcca39cb63356f2cea7d79abf27f284801b3c149d9@820923e0-be08-46f5-a34a-003f91f00f5c-bluemix.cloudant.com/item
+curl -X DELETE https://820923e0-be08-46f5-a34a-003f91f00f5c-bluemix:10d585c237c8d7b599b79cfcca39cb63356f2cea7d79abf27f284801b3c149d9@820923e0-be08-46f5-a34a-003f91f00f5c-bluemix.cloudant.com/review
+
+# Recreate Databases
+curl -X PUT https://820923e0-be08-46f5-a34a-003f91f00f5c-bluemix:10d585c237c8d7b599b79cfcca39cb63356f2cea7d79abf27f284801b3c149d9@820923e0-be08-46f5-a34a-003f91f00f5c-bluemix.cloudant.com/item
+curl -X PUT https://820923e0-be08-46f5-a34a-003f91f00f5c-bluemix:10d585c237c8d7b599b79cfcca39cb63356f2cea7d79abf27f284801b3c149d9@820923e0-be08-46f5-a34a-003f91f00f5c-bluemix.cloudant.com/review
+
+# Populate item Database
+for filename in *.json; do
+  curl -H "Content-Type: application/json" -d @"$filename" https://820923e0-be08-46f5-a34a-003f91f00f5c-bluemix:10d585c237c8d7b599b79cfcca39cb63356f2cea7d79abf27f284801b3c149d9@820923e0-be08-46f5-a34a-003f91f00f5c-bluemix.cloudant.com/item
+done
