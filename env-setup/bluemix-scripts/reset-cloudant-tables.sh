2877b3fba5a90f0007d5e773b8b5571f06b222d8
Tim Baker <bakert@us.ibm.com>
2016-08-29 13:46:12 -0500

    update to merge script

--- env-setup/bluemix-scripts/reset-cloudant-tables.sh
+++ env-setup/bluemix-scripts/reset-cloudant-tables.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-#CLOUDANT="https://820923e0-be08-46f5-a34a-003f91f00f5c-bluemix:10d585c237c8d7b599b79cfcca39cb63356f2cea7d79abf27f284801b3c149d9@820923e0-be08-46f5-a34a-003f91f00f5c-bluemix.cloudant.com"
-CLOUDANT="https://5cebf802-87a1-49c0-a694-4ebf92fa4727-bluemix:b0224893c28a070981be10b189b9f5aebc4dcb6f75b874b5e624b1ce5e24b2bc@5cebf802-87a1-49c0-a694-4ebf92fa4727-bluemix.cloudant.com"
+CLOUDANT="https://820923e0-be08-46f5-a34a-003f91f00f5c-bluemix:10d585c237c8d7b599b79cfcca39cb63356f2cea7d79abf27f284801b3c149d9@820923e0-be08-46f5-a34a-003f91f00f5c-bluemix.cloudant.com"
 
 # Delete Databases
 curl -X DELETE $CLOUDANT/item
