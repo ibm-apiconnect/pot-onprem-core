ed9409601c39f556211f7fdc333b9f83bd166b16
Tim Baker <bakert@us.ibm.com>
2017-01-05 17:30:06 -0600

    updating nginx for datapower ui

--- docker/sni-proxy/run.sh
+++ docker/sni-proxy/run.sh
@@ -4,6 +4,7 @@ cp -f /etc/nginx/nginx.tmpl /etc/nginx/nginx.conf
 sed -i -e "s/APIM/${APIM_DNS}/g" /etc/nginx/nginx.conf
 sed -i -e "s/PORTAL/${PORTAL_DNS}/g" /etc/nginx/nginx.conf
 sed -i -e "s/GATEWAY/${GATEWAY_DNS}/g" /etc/nginx/nginx.conf
+sed -i -e "s/DATAPOWER/${DATAPOWER_DNS}/g" /etc/nginx/nginx.conf
 sed -i -e "s/CONSUMER/${CONSUMER_DNS}/g" /etc/nginx/nginx.conf
 sed -i -e "s/STUBSVCS/${STUBSVCS_DNS}/g" /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf
