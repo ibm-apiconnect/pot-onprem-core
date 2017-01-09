dc9a730f74d37a57ae8dafd3e4916cdf085f5c0a
Tim Baker <bakert@us.ibm.com>
2017-01-09 12:26:12 -0600

    update route name and port for inventory app

--- docker/sni-proxy/run.sh
+++ docker/sni-proxy/run.sh
@@ -7,6 +7,6 @@ sed -i -e "s/APIGATEWAY/${APIGATEWAY_DNS}/g" /etc/nginx/nginx.conf
 sed -i -e "s/DATAPOWER/${DATAPOWER_DNS}/g" /etc/nginx/nginx.conf
 sed -i -e "s/CONSUMER/${CONSUMER_DNS}/g" /etc/nginx/nginx.conf
 sed -i -e "s/STUBSVCS/${STUBSVCS_DNS}/g" /etc/nginx/nginx.conf
-sed -i -e "s/DOCKERHOST/${DOCKERHOST_DNS}/g" /etc/nginx/nginx.conf
+sed -i -e "s/INVENTORYAPP/${INVENTORYAPP_DNS}/g" /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf
 exec nginx -g "daemon off;"
