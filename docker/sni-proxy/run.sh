747ac38eaedb787b447166d33150657222bf0ebf
Tim Baker <bakert@us.ibm.com>
2017-01-09 12:20:10 -0600

    add route to docker host

--- docker/sni-proxy/run.sh
+++ docker/sni-proxy/run.sh
@@ -3,9 +3,10 @@
 cp -f /etc/nginx/nginx.tmpl /etc/nginx/nginx.conf
 sed -i -e "s/APIM/${APIM_DNS}/g" /etc/nginx/nginx.conf
 sed -i -e "s/PORTAL/${PORTAL_DNS}/g" /etc/nginx/nginx.conf
-sed -i -e "s/GATEWAY/${GATEWAY_DNS}/g" /etc/nginx/nginx.conf
+sed -i -e "s/APIGATEWAY/${APIGATEWAY_DNS}/g" /etc/nginx/nginx.conf
 sed -i -e "s/DATAPOWER/${DATAPOWER_DNS}/g" /etc/nginx/nginx.conf
 sed -i -e "s/CONSUMER/${CONSUMER_DNS}/g" /etc/nginx/nginx.conf
 sed -i -e "s/STUBSVCS/${STUBSVCS_DNS}/g" /etc/nginx/nginx.conf
+sed -i -e "s/DOCKERHOST/${DOCKERHOST_DNS}/g" /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf
 exec nginx -g "daemon off;"
